d57daf5ae485011dc23644244389fc941a3ea257
harrydaksh <harinderdaksh01@gmail.com>
2022-03-07 15:49:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// 'use strict';
+
+// // const bookings = [];
+// // const createBooking = function (
+// //   flightNum,
+// //   numPassengers = 1,
+// //   price = numPassengers * 500
+// // ) {
+// //   // numPassengers = numPassengers ||1;
+// //   // price = price ||2600;
+// //   const booking = {
+// //     flightNum,
+// //     numPassengers,
+// //     price,
+// //   };
+// //   console.log(booking);
+// //   bookings.push(booking);
+// // };
+// // createBooking('LH123', '3', '300');
+// // // createBooking('LH002')
+// // console.log(bookings);
+
+// const flight = 'LH123';
+// const user = {
+//     name:"harry daksh",
+//     passport:15935746632,
+// }
+
+// const checkIn = function(flight,user){
+// console.log(flight,user)
+// flight='LJ456'
+// user.name='mr. ' + user.name;
+// console.log(flight,user)
+// if(user.passport === 15935746632){
+//     console.log('checked in')
+// }else{
+//     console.log('unchecked')
+// }
+// }
+// checkIn(flight,user);
+// console.log(flight,user)
+
+// const passenger = user;
+
+// const newPassport = function (user) {
+//     user.passport = Math.trunc(Math.random()*10000000000)
+
+// }
+
+// newPassport(user);
+// console.log(user)
+// checkIn(flight,user)
